2779bf1544bc2475bff36d9ff02149d9ddf77165
Simon LOIR <simon.loir@student.unamur.be>
2023-01-20 18:06:59 +0100

    tests with tailwind

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
         `./pages/**/*.{js,ts,jsx,tsx}`,
         `../../packages/ui/src/**/*.{js,ts,jsx,tsx}`,
     ],
-    theme: {},
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                panel: '250px 1fr',
+            },
+            gridTemplateRows: {
+                panel: '65px 1fr',
+            },
+            fontFamily: {
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+        },
+    },
     plugins: [],
 };
--- packages/ui/src/layout/AdminPanel/AdminPanel.tsx
+++ packages/ui/src/layout/AdminPanel/AdminPanel.tsx
@@ -3,6 +3,11 @@ import Link from 'next/link';
 import { useRef, useState } from 'react';
 import LoginProvider from '../../auth/LoginProvider';
 
+const menuItems = [
+    { name: 'Home', url: '/' },
+    { name: 'Factures', url: '/invoices' },
+];
+
 export default function AdminPanel({
     children,
 }: {
@@ -11,17 +16,29 @@ export default function AdminPanel({
     const ref = useRef<{ logout: () => void }>();
     return (
         <LoginProvider ref={ref}>
-            {children}
-            <button onClick={() => console.log(ref.current?.logout())}>
-                Déconnexion
-            </button>
-
-            <Link
-                href={'/invoices'}
-                className='font-bold underline text-gray-400 p-18 m-10 hover:text-sky-700'
-            >
-                Factures
-            </Link>
+            <div className='fixed grid grid-cols-panel top-0 left-0 right-0 bottom-0 font-montserrat'>
+                <nav className='grid bg-indigo-600 grid-rows-panel'>
+                    <div className='text-white grid justify-center items-center font-semibold text-2xl tracking-wide'>
+                        EZbiz
+                    </div>
+                    <div>
+                        {menuItems.map((item, i) => (
+                            <div
+                                className='text-white p-4 text-medium'
+                                key={item.url}
+                            >
+                                <Link href={item.url}>{item.name}</Link>
+                            </div>
+                        ))}
+                    </div>
+                </nav>
+                <div className='grid grid-rows-panel'>
+                    <header className='leading-[65px] text-xl font-semibold px-5 shadow-lg z-90 tracking-wide'>
+                        Test page
+                    </header>
+                    <main className='p-5 bg-gray-100 z-85'>{children}</main>
+                </div>
+            </div>
         </LoginProvider>
     );
 }
