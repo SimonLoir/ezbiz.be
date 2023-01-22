c0f9464bf3b31786550339eeee5831c3a45a40da
Simon LOIR <simon.loir@student.unamur.be>
2023-01-22 15:55:52 +0100

    added a way to switch db

--- packages/sdk/index.ts
+++ packages/sdk/index.ts
@@ -3,9 +3,14 @@ import Customers from './Customers';
 import Auth from './Auth';
 export default class EZBiz {
     private static database = new Database({
-        host: process.env.DB_HOST ?? 'http://127.0.0.1',
-        port: process.env.DB_PORT ?? '8090',
+        host: 'http://127.0.0.1',
+        port: '8090',
     });
     public static auth = new Auth(EZBiz.database);
     public static customers = new Customers(EZBiz.database);
+    public static set config(config: { host: string; port: string }) {
+        this.database = new Database(config);
+        this.auth = new Auth(this.database);
+        this.customers = new Customers(this.database);
+    }
 }
